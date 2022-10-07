515f0a69bed011912aade7a42e3e5912455bc684
AyakaB <ayaka.bando22@gmail.com>
2022-10-07 13:39:31 -0700

    created useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  return <div>This is a template for React.js</div>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    if (count < 100) setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const increment2 = () => {
+    setCount((previousCount) => previousCount + 1);
+  };
+
+  const decrement2 = () => {
+    setCount((previousCount) => previousCount - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  const double = () => {
+    setCount(count * 2);
+  };
+
+  const three = () => {
+    if (count % 3 === 0) {
+      setCount(count / 3);
+    } else {
+      return count;
+    }
+  };
+
+  return (
+    <>
+      <div>count:{count}</div>
+      <div>
+        <button onClick={increment}>Click Here for increment!</button>
+        <button onClick={decrement}>Click Here for decrement!</button>
+      </div>
+      <div>
+        <button onClick={increment2}>Click Here for increment2!</button>
+        <button onClick={decrement2}>Click Here for decrement2!</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>x2</button>
+        <button onClick={three}>Three</button>
+      </div>
+    </>
+  );
 };
 
 export default App;
