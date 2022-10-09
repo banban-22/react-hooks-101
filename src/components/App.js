9ef7eff7d6aa1ef297281039363b482caf76d2eb
AyakaB <ayaka.bando22@gmail.com>
2022-10-09 11:43:39 -0700

    added useContext

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AppContext from '../contexts/AppContext';
 import reducer from '../reducers/index';
 import Events from './Events';
 import EventForm from './EventForm';
@@ -8,10 +9,12 @@ const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />
-    </div>
+    <AppContext.Provider value={'Hello'}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   );
 };
 
